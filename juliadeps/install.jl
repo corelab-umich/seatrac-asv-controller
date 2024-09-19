dc065e11e62257104091183fd4fd97099a55a60d
kmgovind <kavin.gmk@gmail.com>
2024-09-19 15:55:06 -0400

    Added julia source code

---
+++ juliadeps/install.jl
@@ -0,0 +1,3 @@
+using Pkg
+pkg"instantiate"
+pkg"precompile"
---
+++ src/asv_controller/asv_controller/juliatest.py
@@ -0,0 +1,48 @@
+import rclpy
+from rclpy.node import node
+from juliacall import Main as jl
+import numpy as np
+
+from std_msgs.msg import String
+
+class JuliaPublisher(Node):
+
+    def __init__(Node):
+        super().__init__('julia_publisher')
+
+        """ SOC Controller Testing"""
+        self.soc_controller = jl.include("jl_src/SOC_Controller.jl")
+        dt_sec = 2.5
+        dt_min = dt_sec/(60.0)
+        self.dt_hrs = dt_sec/(60.0 * 60.0)
+        T_begin = 9.0
+        T_end = 12.0
+        self.ts_hrs = [T_begin + i * dt_Hrs for i in range(int((T_end - T_begin)/ dt_hrs) + 1)]
+
+        """ ROS Publisher"""
+        self.publisher_ = self.create_publisher(String, 'julia_msg', 10)
+        timer_period = 0.5 # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+    
+    def timer_callback(self):
+
+        lcbf = self.soc_controller.compute_lcbf(self.ts_hrs, self.dt_hrs)
+        msg = String()
+        msg.data = String(lcbf[2])
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "$s"' % msg.data)
+        self.i += 1
+
+def main(args=None):
+    rclpy.init(args=args)
+    
+    jl_publisher = JuliaPublisher()
+
+    rclpy.spin(jl_publisher)
+
+    jl_publisher.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
