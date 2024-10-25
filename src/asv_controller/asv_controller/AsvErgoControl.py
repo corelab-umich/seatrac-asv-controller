Fixed heading calculation error; now computes bearing with 0 being north

src/asv_controller/asv_controller/AsvErgoControl.py
@@ -318,7 +318,7 @@ class ASVErgoControl(Node):
 
    
     def heading_calc(self, ux, uy):
-        heading = np.arctan2(uy, ux) * (180/np.pi)
+        heading = np.degrees(np.atan2(ux, uy))
         return (heading + 360) % 360
 
 def main(args=None):

src/asv_controller/asv_controller/juliatest.py
@@ -141,8 +141,8 @@ class JuliaPublisher(Node):
             heading = self.heading_command
 
             # update x/y position
-            self.pose_x += (speed*np.cos(np.radians(heading))) * self.timer_period * K_M2KM
-            self.pose_y += (speed*np.sin(np.radians(heading))) * self.timer_period * K_M2KM
+            self.pose_x += (speed*np.sin(np.radians(heading))) * self.timer_period * K_M2KM
+            self.pose_y += (speed*np.cos(np.radians(heading))) * self.timer_period * K_M2KM
             new_data.pose_x = self.pose_x
             new_data.pose_y = self.pose_y
 