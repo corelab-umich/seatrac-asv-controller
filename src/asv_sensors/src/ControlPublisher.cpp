19d42ba9ac99665250d6d99387c7e0f2914d66f3
kmgovind <kavin.gmk@gmail.com>
2024-10-28 21:18:56 -0400

    Added RPM interpolation in command packet publisher

--- src/asv_sensors/src/ControlPublisher.cpp
+++ src/asv_sensors/src/ControlPublisher.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <cmath>
 #include <random>
+#include <vector>
 
 // other headers
 #include <rclcpp/rclcpp.hpp>
@@ -20,6 +21,19 @@ using namespace asv::networking;
 
 #define LOCALHOST "127.0.0.2"
 #define PORT 62002
+
+// A struct to hold each data point (RPM, Speed in knots)
+struct DataPoint {
+    float rpm;
+    float speed;
+};
+
+// Table data
+std::vector<DataPoint> table = {
+    {0, 0.0}, {200, 0.7}, {400, 1.6}, {500, 2.0},
+    {600, 2.5}, {700, 2.9}, {800, 3.3}, {900, 3.6},
+    {1000, 4.0}, {1100, 4.3}, {1200, 4.6}, {1300, 5.0}
+};
 
 
 class ControlPublisher : public rclcpp::Node
@@ -50,9 +64,21 @@ private:
 
     float kts_to_rpm(float kts)
     {
-        //TODO: Implement interpolation
-        float rpm = kts * 0.0;
-        return rpm;
+
+      // Ensure the target speed is within the range of the table
+      if (kts <= table.front().speed) return table.front().rpm;
+      if (kts >= table.back().speed) return table.back().rpm;
+
+      // Find the two data points that bracket the target speed
+      for (size_t i = 0; i < table.size() - 1; ++i) {
+          if (table[i].speed <= kts && kts <= table[i + 1].speed) {
+              // Manual linear interpolation for RPM
+              double ratio = (kts - table[i].speed) / (table[i + 1].speed - table[i].speed);
+              return  float(static_cast<int>(table[i].rpm + ratio * (table[i + 1].rpm - table[i].rpm)));
+          }
+      }
+      // Return -1 if something goes wrong (shouldn't happen with valid data)
+      return -1;
     }
 
     rclcpp::Subscription<::messages::msg::Commands>::SharedPtr control_sub_;
