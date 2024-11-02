f9a344b8750bf579296a857ce3738e11b98106b2
kmgovind <kavin.gmk@gmail.com>
2024-11-02 12:20:57 -0400

    changed command publisher rate to 0.5 s from 1s

--- src/asv_controller/asv_controller/AsvErgoControl.py
+++ src/asv_controller/asv_controller/AsvErgoControl.py
@@ -159,7 +159,7 @@ class ASVErgoControl(Node):
 
         """ Control Loop on Timer """
         self.publisher_ = self.create_publisher(Commands, 'asv_command', 10)
-        timer_period = 1 # seconds
+        timer_period = 0.5 # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
     
     def timer_callback(self):
